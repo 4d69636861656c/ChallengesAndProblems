e145b4f7a582179584a308bcf2971aed187e88eb
Iordache Mihail <iordache.mihail.madalin@gmail.com>
2020-03-24 20:57:55 +0200

    Updated the String Manipulation library to include operations on individual words. Added a project which demonstrates how to separate the individual words in a random string and calculate which word has the highest score. The score of a character is calculated by taking the order in the English alphabet of a letter. The score of a word is the sum of the score of each letter in a word. The highest score word is returned from the given string.

--- StringUtilities/StringUtilities.cs
+++ StringUtilities/StringUtilities.cs
@@ -1,7 +1,11 @@
-﻿namespace StringManipulation
+﻿using System;
+
+namespace StringManipulation
 {
     public static class StringUtilities
     {
+        private static char[] wordSeparators = { ' ', '.', ',', '|' };
+
         public static string InvertCase(string text)
         {
             char[] characters = text.ToCharArray();
@@ -24,6 +28,84 @@
             }
 
             return resultingText;
+        }
+
+        public static string[] SplitIntoWords(string text)
+        {
+            string trimmedText = text.Trim();
+            int numberOfWords = CountNumberOfWords(trimmedText);
+            string[] words = new string[numberOfWords];
+
+            if (numberOfWords == 0)
+            {
+                throw new InvalidOperationException("String is empty.");
+            }
+
+            int currentWord = 0;
+
+            for (int i = 0; i < trimmedText.Length; i++)
+            {
+                if ((i < trimmedText.Length - 1)
+                    && !((trimmedText[i] >= 'a' && trimmedText[i] <= 'z') || (trimmedText[i] >= 'A' && trimmedText[i] <= 'Z'))
+                    && ((trimmedText[i + 1] >= 'a' && trimmedText[i + 1] <= 'z') || (trimmedText[i + 1] >= 'A' && trimmedText[i + 1] <= 'Z')))
+                {
+                    currentWord++;
+                }
+                else if ((trimmedText[i] >= 'a' && trimmedText[i] <= 'z') || (trimmedText[i] >= 'A') && trimmedText[i] <= 'Z')
+                {
+                    words[currentWord] += trimmedText[i];
+                }
+            }
+
+            return words;
+        }
+
+        public static int CountNumberOfWords(string text)
+        {
+            int wordCounter;
+            string trimmedText = text.Trim();
+
+
+            if (text.Length == 0)
+            {
+                // Throwing an exception would also work here. 
+                wordCounter = 0;
+            }
+            else
+            {
+                wordCounter = 1;
+            }
+
+            for (int i = 0; i < trimmedText.Length; i++)
+            {
+                if ((i < trimmedText.Length - 1)
+                    && !((trimmedText[i] >= 'a' && trimmedText[i] <= 'z') || (trimmedText[i] >= 'A' && trimmedText[i] <= 'Z'))
+                    && ((trimmedText[i + 1] >= 'a' && trimmedText[i + 1] <= 'z') || (trimmedText[i + 1] >= 'A' && trimmedText[i + 1] <= 'Z')))
+                {
+                    wordCounter++;
+                }
+            }
+
+            return wordCounter;
+        }
+
+        public static int CalculateWordScore(string text)
+        {
+            int wordScore = 0;
+
+            foreach (char character in text)
+            {
+                if (character >= 'A' && character <= 'Z')
+                {
+                    wordScore += (int)character - 64;
+                }
+                else if (character >= 'a' && character <= 'z')
+                {
+                    wordScore += (int)character - 96;
+                }
+            }
+
+            return wordScore;
         }
     }
 }
---
+++ WordScore/EntryPoint.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static StringManipulation.StringUtilities;
+
+// Demonstrates how to separate the individual words in a random string and calculate which word has the highest score.
+// The score of a character is calculated by taking the order in the English alphabet of a letter.
+//The score of a word is the sum of the score of each letter in a word. 
+//The highest score word is returned from the given string.
+
+namespace WordScore
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            DoWork();
+        }
+
+        private static void DoWork()
+        {
+            string text = "   This, this is ... A random string! I hope this program will compile and run. \\0/   ";
+            Console.WriteLine($"The given string is: \"{text}\"");
+
+            Console.WriteLine($"The string has {CountNumberOfWords(text)} words.");
+
+            string[] words = SplitIntoWords(text);
+
+            Console.WriteLine("The words and their calculated points are as follows: ");
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. The word \"{words[i]}\", with an associated score of {CalculateWordScore(words[i])}.");
+            }
+
+            // Compare scores and decide the winner. 
+            int score = 0;
+            int position = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (CalculateWordScore(words[i]) > score)
+                {
+                    score = CalculateWordScore(words[i]);
+                    position = i;
+                }
+            }
+
+            Console.WriteLine($"The word with the highest score is \"{words[position]}\", with a score of {CalculateWordScore(words[position])} points.");
+        }
+    }
+}
