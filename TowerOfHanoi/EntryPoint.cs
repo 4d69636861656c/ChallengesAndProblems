dcabcdaff1ae487e4e6f02075b12badb5d660f0f
Iordache Mihail <iordache.mihail.madalin@gmail.com>
2020-03-28 21:57:00 +0200

    Added the Tower Of Hanoi project to the solution.

---
+++ TowerOfHanoi/EntryPoint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TowerOfHanoi
+{
+    class EntryPoint
+    {
+        private static int counter = 0;
+
+        static void Main()
+        {
+            Console.Write("Please enter the number of discs: ");
+            string userInput = Console.ReadLine();
+            if (Int32.TryParse(userInput, out int discs))
+            {
+                Tower(discs, 1, 3, 2);
+            }
+            else
+            {
+                Console.WriteLine("The input needs to be a number. Please try again.");
+            }
+        }
+
+        private static void Tower(int n, int sourcePeg, int destinationPeg, int sparePeg)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine($"{counter}. {sourcePeg} -> {destinationPeg}");
+                counter++;
+            }
+            else
+            {
+                Tower(n - 1, sourcePeg, sparePeg, destinationPeg);
+                Console.WriteLine($"{counter}. {sourcePeg} -> {destinationPeg}");
+                counter++;
+                Tower(n - 1, sparePeg, destinationPeg, sourcePeg);
+            }
+        }
+    }
+}
